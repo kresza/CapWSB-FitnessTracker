fb6668ed3737060e3690a642c49619f2406d5080
Jakub Kryska <jkryska@pgl.pl>
2025-11-08 16:49:54 +0100

    lab2: create relations and health metrics entity

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -1,11 +1,18 @@
 package pl.wsb.fitnesstracker.statistics.api;
 
-import jakarta.annotation.Nullable;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
 @Table(name = "Statistics")
@@ -16,19 +23,24 @@ public class Statistics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Nullable
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(name = "total_trainings", nullable = false)
     private int totalTrainings;
 
-    @Column(name = "total_distance")
+    @Column(name = "total_distance", nullable = false)
     private double totalDistance;
 
-    @Column(name = "total_calories_burned")
+    @Column(name = "total_calories_burned", nullable = false)
     private int totalCaloriesBurned;
 
-    public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+    public Statistics(
+            User user, int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+        this.user = user;
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
--- src/main/java/pl/wsb/fitnesstracker/user/api/User.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/User.java
@@ -6,8 +6,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.healthmetrics.HealthMetrics;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -21,11 +23,20 @@ public class User {
     @Nullable
     private Long id;
 
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
     @Column(name = "birthdate", nullable = false)
     private LocalDate birthdate;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<HealthMetrics> healthMetrics;
 
     public User(
             final String firstName,
@@ -33,6 +44,8 @@ public class User {
             final LocalDate birthdate,
             final String email) {
 
+        this.lastName = lastName;
+        this.firstName = firstName;
         this.birthdate = birthdate;
         this.email = email;
     }
